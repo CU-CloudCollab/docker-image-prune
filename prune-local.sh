adabc9c319c9f49722a975c9b744c97015b393bb
Paul Allen <pea1@cornell.edu>
2016-04-20 17:01:59 -0400

    add argument handling

--- prune-local.sh
+++ prune-local.sh
@@ -4,7 +4,28 @@
 # It utilizes the same expiration logic as prune-dtr.rb, but here the expiration age
 # is set very small because we want to expire all but the three most recent tags.
 #
-EXPIRATION_DAYS=90
+# Examples:
+#   ./prune-local.sh       -- uses default expiration age of 90 days
+#   ./prune-local.sh 30    -- set expriation age to 30 DEFAULT_EXPIRATION_AGE_DAYS
+
+
+# Very simple argument handling
+if [[ $# -ne 1 && $# -ne 0 ]]; then
+  echo "usage: $0 [age-days]"
+  exit 1
+fi
+RE='^[0-9]+$'
+if [[ $# -eq 1 ]] ; then
+  if [[ ($1 =~ $RE) ]] ; then
+    EXPIRATION_DAYS=$1
+  else
+    echo "Expiration age in days must be a positive, whole number."
+    exit 1
+  fi
+else
+  EXPIRATION_DAYS=90
+fi
+echo "Using expiration age of $EXPIRATION_DAYS days."
 
 # Get the list of unique repos that are present locally
 for REPO in $(docker images --filter "dangling=false" --format '{{.Repository}}' | uniq)
